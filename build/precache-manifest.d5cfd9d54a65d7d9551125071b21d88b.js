e09c7bb424fb8785f2a67e0823f7a53ade940f88
varkum <>
2021-05-04 03:46:22 +0000

    added build

---
+++ build/precache-manifest.d5cfd9d54a65d7d9551125071b21d88b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb2ab2cbe95cd374d790726f3886f6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8920f4bf4c4aaff36277",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "fc90f17afcf7f301a5f2",
+    "url": "/static/css/main.e238e11f.chunk.css"
+  },
+  {
+    "revision": "8920f4bf4c4aaff36277",
+    "url": "/static/js/2.a304febe.chunk.js"
+  },
+  {
+    "revision": "fc90f17afcf7f301a5f2",
+    "url": "/static/js/main.d7ce23c7.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
